67eb0decb520c490a3dcddcf899497ba806d2b0c
Joe McArthur <joe@openaccessbutton.org>
2023-03-22 18:02:59 +0000

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,36 @@
+// Replace with your own values
+const searchClient = algoliasearch(
+  'J448NRRMMY',
+  'd55ee251fd815bdda97dfb3732fbeff9' // search only API key, not admin API key
+)
+
+const search = instantsearch({
+  indexName: 'contacts',
+  searchClient,
+  routing: true,
+})
+
+search.addWidgets([
+  instantsearch.widgets.configure({
+    hitsPerPage: 10,
+  }),
+])
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: 'Search for contacts',
+  }),
+])
+
+search.addWidgets([
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: document.getElementById('hit-template').innerHTML,
+      empty: `We didn't find any results for the search <em>"{{query}}"</em>`,
+    },
+  }),
+])
+
+search.start()
