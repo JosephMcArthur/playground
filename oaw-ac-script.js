f5ac2b316301acabde84f041e11106c76085d2fb
Joe McArthur <joe@openaccessbutton.org>
2023-03-30 12:16:21 +0100

    Update oaw-ac-script.js

--- oaw-ac-script.js
+++ oaw-ac-script.js
@@ -1,6 +1,5 @@
 const searchBox = document.getElementById('searchBox');
 const suggestionsList = document.getElementById('suggestionsList');
-let selectedIndex = -1;
 
 searchBox.addEventListener('input', async (event) => {
     const searchTerm = event.target.value;
@@ -8,7 +7,6 @@ searchBox.addEventListener('input', async (event) => {
     if (!searchTerm.trim()) {
         suggestionsList.innerHTML = '';
         suggestionsList.style.display = 'none';
-        selectedIndex = -1;
         return;
     }
 
@@ -22,44 +20,13 @@ searchBox.addEventListener('input', async (event) => {
         if (Array.isArray(filteredData) && filteredData.some(item => item.hasOwnProperty('name'))) {
             suggestionsList.innerHTML = filteredData.map(result => `<li><a href="https://oa.report/${result.objectID}">${result.name}</a></li>`).join('');
             suggestionsList.style.display = 'block';
-            selectedIndex = -1;
         } else {
             suggestionsList.innerHTML = '<li>No results! If you think there should be, <a href="mailto:contact@yourdomain.com">get in touch</a>.</li>';
             suggestionsList.style.display = 'block';
-            selectedIndex = -1;
         }
     } catch (error) {
         console.error('Error fetching suggestions:', error);
     }
-});
-
-searchBox.addEventListener('keydown', (event) => {
-    const items = suggestionsList.getElementsByTagName('li');
-    const maxIndex = items.length - 1;
-
-    if (event.key === 'ArrowDown') {
-        event.preventDefault();
-        selectedIndex = (selectedIndex === maxIndex) ? 0 : selectedIndex + 1;
-    } else if (event.key === 'ArrowUp') {
-        event.preventDefault();
-        selectedIndex = (selectedIndex === 0) ? maxIndex : selectedIndex - 1;
-    } else if (event.key === 'Enter') {
-        event.preventDefault();
-        if (selectedIndex !== -1) {
-            window.location.href = items[selectedIndex].firstElementChild.href;
-        }
-        return;
-    } else {
-        return;
-    }
-
-    Array.from(items).forEach((item, index) => {
-        if (index === selectedIndex) {
-            item.firstElementChild.focus();
-        } else {
-            item.firstElementChild.blur();
-        }
-    });
 });
 
 suggestionsList.addEventListener('click', (event) => {
