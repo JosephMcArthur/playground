4451d326017cdd50bc64d2548f34d00b19d4a15d
Joe McArthur <joe@openaccessbutton.org>
2023-03-30 10:19:51 +0100

    Create oaw-ac-script.js

---
+++ oaw-ac-script.js
@@ -0,0 +1,43 @@
+const searchBox = document.getElementById('searchBox');
+const suggestionsList = document.getElementById('suggestionsList');
+
+searchBox.addEventListener('input', async (event) => {
+    const searchTerm = event.target.value;
+
+    if (!searchTerm.trim()) {
+        suggestionsList.innerHTML = '';
+        suggestionsList.style.display = 'none';
+        return;
+    }
+
+    try {
+        const url = `https://bg.beta.oa.works/report/orgs/suggest/name/?q=${searchTerm}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data && data.results && data.results.length > 0) {
+            suggestionsList.innerHTML = data.results.map(result => `<li>${result.name}</li>`).join('');
+            suggestionsList.style.display = 'block';
+        } else {
+            suggestionsList.innerHTML = '';
+            suggestionsList.style.display = 'none';
+        }
+    } catch (error) {
+        console.error('Error fetching suggestions:', error);
+    }
+});
+
+suggestionsList.addEventListener('click', (event) => {
+    if (event.target.tagName.toLowerCase() === 'li') {
+        searchBox.value = event.target.innerText;
+        suggestionsList.innerHTML = '';
+        suggestionsList.style.display = 'none';
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (event.target !== searchBox) {
+        suggestionsList.innerHTML = '';
+        suggestionsList.style.display = 'none';
+    }
+});
