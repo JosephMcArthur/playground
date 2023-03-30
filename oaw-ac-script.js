91c4b2c726448c7f6c7c6557c82ef686f36182d4
Joe McArthur <joe@openaccessbutton.org>
2023-03-30 11:17:13 +0100

    Update oaw-ac-script.js

--- oaw-ac-script.js
+++ oaw-ac-script.js
@@ -1,5 +1,6 @@
 const searchBox = document.getElementById('searchBox');
 const suggestionsList = document.getElementById('suggestionsList');
+let selectedIndex = -1;
 
 searchBox.addEventListener('input', async (event) => {
     const searchTerm = event.target.value;
@@ -7,6 +8,7 @@ searchBox.addEventListener('input', async (event) => {
     if (!searchTerm.trim()) {
         suggestionsList.innerHTML = '';
         suggestionsList.style.display = 'none';
+        selectedIndex = -1;
         return;
     }
 
@@ -18,13 +20,44 @@ searchBox.addEventListener('input', async (event) => {
         if (Array.isArray(data) && data.some(item => item.hasOwnProperty('name'))) {
             suggestionsList.innerHTML = data.map(result => `<li><a href="https://oa.report/${result.objectID}">${result.name}</a></li>`).join('');
             suggestionsList.style.display = 'block';
+            selectedIndex = -1;
         } else {
             suggestionsList.innerHTML = '';
             suggestionsList.style.display = 'none';
+            selectedIndex = -1;
         }
     } catch (error) {
         console.error('Error fetching suggestions:', error);
     }
+});
+
+searchBox.addEventListener('keydown', (event) => {
+    const items = suggestionsList.getElementsByTagName('li');
+    const maxIndex = items.length - 1;
+
+    if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        selectedIndex = (selectedIndex === maxIndex) ? 0 : selectedIndex + 1;
+    } else if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        selectedIndex = (selectedIndex === 0) ? maxIndex : selectedIndex - 1;
+    } else if (event.key === 'Enter') {
+        event.preventDefault();
+        if (selectedIndex !== -1) {
+            window.location.href = items[selectedIndex].firstElementChild.href;
+        }
+        return;
+    } else {
+        return;
+    }
+
+    Array.from(items).forEach((item, index) => {
+        if (index === selectedIndex) {
+            item.firstElementChild.focus();
+        } else {
+            item.firstElementChild.blur();
+        }
+    });
 });
 
 suggestionsList.addEventListener('click', (event) => {
@@ -32,6 +65,7 @@ suggestionsList.addEventListener('click', (event) => {
         searchBox.value = event.target.innerText;
         suggestionsList.innerHTML = '';
         suggestionsList.style.display = 'none';
+        selectedIndex = -1;
     }
 });
 
@@ -39,5 +73,6 @@ document.addEventListener('click', (event) => {
     if (event.target !== searchBox) {
         suggestionsList.innerHTML = '';
         suggestionsList.style.display = 'none';
+        selectedIndex = -1;
     }
 });
